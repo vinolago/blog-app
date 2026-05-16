2140592d88483ebd70c823ec0e1c3988d2c3315d
vinolago <dr.plancksolago@gmail.com>
2026-05-16 18:25:34 +0300

    table import error fixed

--- client/src/components/ui/MediumEditor.jsx
+++ client/src/components/ui/MediumEditor.jsx
@@ -6,10 +6,10 @@ import Typography from "@tiptap/extension-typography";
 import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
-import Table from "@tiptap/extension-table";
-import TableRow from "@tiptap/extension-table-row";
-import TableCell from "@tiptap/extension-table-cell";
-import TableHeader from "@tiptap/extension-table-header";
+import { Table } from "@tiptap/extension-table";
+import { TableRow } from "@tiptap/extension-table-row";
+import { TableCell } from "@tiptap/extension-table-cell";
+import { TableHeader } from "@tiptap/extension-table-header";
 import sanitizeHtml from "sanitize-html";
 import { 
   Bold, Italic, Strikethrough,
