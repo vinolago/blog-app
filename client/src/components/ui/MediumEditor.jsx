e0641af9b450c9c552852acb1ae674c2db5ad306
vinolago <dr.plancksolago@gmail.com>
2026-05-16 18:05:20 +0300

    table import error fixed

--- client/src/components/ui/MediumEditor.jsx
+++ client/src/components/ui/MediumEditor.jsx
@@ -7,9 +7,9 @@ import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
 import Table from "@tiptap/extension-table";
-import TableRow from "@tiptap/extension-table-row";
-import TableCell from "@tiptap/extension-table-cell";
-import TableHeader from "@tiptap/extension-table-header";
+import { TableRow } from "@tiptap/extension-table-row";
+import { TableCell } from "@tiptap/extension-table-cell";
+import { TableHeader } from "@tiptap/extension-table-header";
 import sanitizeHtml from "sanitize-html";
 import { 
   Bold, Italic, Strikethrough,
